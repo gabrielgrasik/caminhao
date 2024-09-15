82592a54bb5d2245da341538433a47f1d7155006
gabrielgrasik <gabrielgrasik@gmail.com>
2024-09-15 16:13:24 -0300

    teste caminhao

---
+++ Testes3.py
@@ -0,0 +1,9 @@
+from Carro import Carro
+from Moto import Moto
+from Caminhao import Caminhao
+kwid = Carro("Renault", "Kwid", "DDF", 2010, 4)
+biz = Moto("Honda", "Biz", "EFI", 2020, 100)
+mercedez = Caminhao("Mercedez", "Actroz","DFG", 2020, 32000)
+print(kwid)
+print(biz)
+print(mercedez)
